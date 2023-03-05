1344150209c7b3fba3db446e851dd1464982d3e7
Pablo Nuñez <pnunfe@gmail.com>
2023-03-05 18:40:29 +0000

    Navigation Drawer

--- app/src/main/java/com/example/jetpackcatalog/ui/theme/Scaffold.kt
+++ app/src/main/java/com/example/jetpackcatalog/ui/theme/Scaffold.kt
@@ -1,11 +1,16 @@
 package com.example.jetpackcatalog.ui.theme
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -15,29 +20,30 @@ fun MyScaffoldExample() {
     val coroutineScope = rememberCoroutineScope()
 
     Scaffold(topBar = {
-        MyTopAppBar {
+        MyTopAppBar(onClickIcon = {
             coroutineScope.launch {
                 scaffoldState.snackbarHostState.showSnackbar(
                     "Has pulsado $it"
                 )
             }
-        }
+        }, onClickDrawer = { coroutineScope.launch { scaffoldState.drawerState.open() } })
     },
         scaffoldState = scaffoldState,
-        bottomBar = { MyBottomBar() }) {
+        bottomBar = { MyBottomBar() },
+        drawerContent = { MyDrawer() }) {
     }
 }
 
 
 @Composable
-fun MyTopAppBar(onClickIcon: (String) -> Unit) {
+fun MyTopAppBar(onClickIcon: (String) -> Unit, onClickDrawer: () -> Unit) {
     TopAppBar(
         title = { Text(text = "ToolBar APP") },
         backgroundColor = Color.DarkGray,
         contentColor = Color.White,
         navigationIcon = {
-            IconButton(onClick = { onClickIcon("Atras") }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
+            IconButton(onClick = { onClickDrawer() }) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
 
             }
         },
@@ -55,16 +61,16 @@ fun MyTopAppBar(onClickIcon: (String) -> Unit) {
 
 @Composable
 fun MyBottomBar() {
-    var index by remember {mutableStateOf(0)}
+    var index by remember { mutableStateOf(0) }
 
     BottomNavigation(backgroundColor = Color.DarkGray, contentColor = Color.White) {
-        BottomNavigationItem(selected = index == 0, onClick = { index = 0}, icon = {
+        BottomNavigationItem(selected = index == 0, onClick = { index = 0 }, icon = {
             Icon(
                 imageVector = Icons.Default.Home,
                 contentDescription = "Home Buton"
             )
         }, label = { Text(text = "Home") })
-        BottomNavigationItem(selected = index == 1, onClick = { index = 1}, icon = {
+        BottomNavigationItem(selected = index == 1, onClick = { index = 1 }, icon = {
             Icon(
                 imageVector = Icons.Default.Favorite,
                 contentDescription = "Favorite Buton"
@@ -76,5 +82,32 @@ fun MyBottomBar() {
                 contentDescription = "Person Button"
             )
         }, label = { Text(text = "Favorite") })
+    }
+}
+
+@Composable
+fun MyDrawer() {
+    Column(Modifier.padding(8.dp)) {
+        Text(
+            text = "Primera Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+        Text(
+            text = "Segunda Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+        Text(
+            text = "Tercera Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+        Text(
+            text = "Cuarta Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+
     }
 }
